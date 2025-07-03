3af3032c007a54a224b3f097b1284846a1232b73
aparnajoseph2024 <aparnajoseph.2024@gmail.com>
2025-07-04 09:11:42 +1000

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,13 @@ import sys
 import dill
 import pandas as pd
 import numpy as np
+import pickle
 
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
+from src.logger import logging
 
 def save_object(file_path, obj):
     try:
@@ -18,12 +22,19 @@ def save_object(file_path, obj):
     except:
         pass
 
-def evaluate_models(X_train, Y_train, X_test, Y_test, models):
+def evaluate_models(X_train, Y_train, X_test, Y_test, models, param):
     try:
         report = {}
         for i in range(len(list(models))):
-            model = list(models.values())[i]
+            logging.info(f"Fitting model {list(models.keys())[i]}...")
 
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, Y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, Y_train)
 
             Y_train_pred = model.predict(X_train)
@@ -34,6 +45,14 @@ def evaluate_models(X_train, Y_train, X_test, Y_test, models):
 
             report[list(models.keys())[i]] = test_model_score
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
     except Exception as e:
         raise CustomException(e, sys)
